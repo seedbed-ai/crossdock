fd175a56247a5ca973e4c36aa146ca3b353689b4
Seedbed Owner <311109535+seedbed-ai-owner@users.noreply.github.com>
2026-08-31 20:36:59 -0400

    feat: export service endpoint helpers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 export { CONFIG_SCHEMA, CONFIG_SCOPES, DEFAULT_CONFIG, HANDOFF_MODES, effectiveConfigSummary, resolveConfig, validateConfig } from "./config.js";
 export { GitHubClient } from "./github-client.js";
 export { buildPrBody, buildUpdateComment, persistTaskRecord, publishExistingInitialHandoff, publishInitialHandoff, publishUpdateHandoff } from "./handoff.js";
+export { createHandoffServer } from "./http-server.js";
 export { assertGithubSafe } from "./security.js";
+export { DEFAULT_SERVICE_URL, normalizeServiceUrl, parseServicePort } from "./service-endpoint.js";
+export { dispatchHandoff, hydrateTaskFromPullRequest } from "./service.js";
 export { TASK_RECORD_STORAGE_ADAPTER, createGitHubTaskRecordStorage, isTaskRecordStorageAdapter, resolveTaskRecordStorage } from "./storage.js";
 export { EVIDENCE_MODES, TASK_RECORD_SCHEMA, canonicalizeText, evidencePolicy, renderTaskRecord, sha256, taskRecordPath, validateTaskRecord } from "./task-record.js";
-export { createHandoffServer } from "./http-server.js";
-export { dispatchHandoff, hydrateTaskFromPullRequest } from "./service.js";
